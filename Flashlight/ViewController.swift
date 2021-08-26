66a35301ea11ae078bf172a31bc3d2df99e76f2a
Harrison Hall <harrison.hall23@d155.org>
2021-08-26 07:44:49 -0500

    Initial commit (flashlight app)

---
+++ Flashlight/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Flashlight
+//
+//  Created by HARRISON HALL on 8/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var onButtonOut: UIButton!
+    @IBOutlet weak var offButtonOut: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func offButton(_ sender: Any) {
+        view.backgroundColor = UIColor.black
+        mode.text = "Off"
+        
+       // offButtonOut.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    }
+    
+    
+    @IBAction func onButton(_ sender: Any) {
+        view.backgroundColor = UIColor.white
+        mode.text = "On"
+      //  onButtonOut.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        
+    }
+    
+    @IBOutlet weak var mode: UILabel!
+    
+    
+    
+    
+}
